58e841b8c2bc32134b4839bbbb0a481ec61aba91
5-6-1 <5-6-1@users.noreply.github.com>
2026-08-21 03:51:07 +0800

    chore: ignore target and lockfile

--- target/debug/deps/alga2_derive-1a9199ca8bfc875e.d
+++
@@ -1,5 +0,0 @@
-D:\CS\Rust\alga2-derive-rs\target\debug\deps\alga2_derive-1a9199ca8bfc875e.d: src\lib.rs
-
-D:\CS\Rust\alga2-derive-rs\target\debug\deps\alga2_derive-1a9199ca8bfc875e.exe: src\lib.rs
-
-src\lib.rs:
